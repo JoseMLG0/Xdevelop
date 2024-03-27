f35ced62475d1219b7b51f99c3fed3087b8dc6f3
JoseMLG0 <52148643+JoseMLG0@users.noreply.github.com>
2024-03-27 14:45:19 -0600

    Emcriptacion de password

--- src/config/db.init.ts
+++ src/config/db.init.ts
@@ -2,7 +2,7 @@ import { Users } from "../models/users.model";
 
 const dbInit = async () => {
   const isDev = process.env.NODE_ENV === "development";
-  await Users.sync({ alter: false });
+  await Users.sync({ alter: isDev });
 };
 
 export default dbInit;
--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -2,13 +2,17 @@ import { Router, Request, Response } from "express";
 import UsersServices from "./../services/users.service";
 import multer, { Multer } from "multer";
 import fs from "fs";
-
+import CryptoJS from 'crypto-js'
 export default class UserController {
   private usersRouter: Router;
   private uploadFiles?: Multer;
   private uploadDirectory = process.env.PROFILES_PICTURES || "/uploads";
+  private secretKey = process.env.SECRET_KEY || '';
 
   constructor() {
+    if(!this.secretKey){
+      throw new Error("SE REQUIERE DEFINIR UNA SECRET_KEY EN LAS VARIABLES DE ENTORNO");
+    }
     this.usersRouter = Router();
     this.configFolderFiles();
     this.routes();
@@ -66,7 +70,7 @@ export default class UserController {
           newData.lastname = apellido;
           newData.email = correo;
           if(!!contrasena){
-            newData.password = contrasena;
+            newData.password = CryptoJS.AES.encrypt(contrasena, this.secretKey).toString();
           }
 
           const id = Number(req.params.id);
@@ -83,9 +87,8 @@ export default class UserController {
           }
 
           const result = await UsersServices.update(id, newData);
-          console.log(result)
+          
           if(!!result){
-            console.log(this.uploadDirectory+getDataCurrent.picture)
             if (fs.existsSync(this.uploadDirectory+getDataCurrent.picture)) {
               fs.unlinkSync(this.uploadDirectory+getDataCurrent.picture)
             }
@@ -118,7 +121,7 @@ export default class UserController {
           const nombre = req.body.nombre;
           const apellido = req.body.apellido;
           const correo = req.body.correo;
-          const contrasena = req.body.contrasena;
+          const contrasena = CryptoJS.AES.encrypt(req.body.contrasena, this.secretKey).toString();
 
           // Archivo enviado
           const imagen = req.file;
@@ -131,6 +134,7 @@ export default class UserController {
           });
           res.json(user);
         } catch (error) {
+          console.log(error);
           res.status(500).json({ message: "Error al crear el usuario." });
         }
       }
